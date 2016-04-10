574e80ed337467f93a72758236025f7f121c6269
Ha Duong <duong_minh_ha@yahoo.de>
2016-04-10 23:24:51 +0100

    Home update

--- js/main.js
+++ js/main.js
@@ -181,8 +181,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 
-	    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
-
+	    var myLatLng = new google.maps.LatLng(49.006872, 8.403620);
 	    var mapOptions = {
 	        zoom: 15,
 	        center: myLatLng,
@@ -253,7 +252,7 @@ function init() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(22.402789, 91.822156),
+        position: new google.maps.LatLng(49.006872, 8.403620),
         map: map,
 		icon: 'img/icons/map-marker.png',
     });
